7ec7448f0222e8e949ed2611d91ea595bf539258
ValentinKrasovski <92800455+ValentinKrasovski@users.noreply.github.com>
2022-03-09 23:57:15 +0300

    Add

---
+++ Lab 1/_153501_Krasovski/_153501_Krasovski/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _153501_Krasovski
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.WriteLine("Enter the first number: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            c = a / b;
+            Console.WriteLine("The result: " + c);
+        }
+    }
+}
