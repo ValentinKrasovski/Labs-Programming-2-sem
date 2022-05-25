cffac6c12ce5343ed120ee79297fc0df4b799d84
ValentinKrasovski <92800455+ValentinKrasovski@users.noreply.github.com>
2022-05-26 02:09:49 +0300

    Add files via upload

---
+++ Lab 6/Lab6Tests/StarTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6.Tests
+{
+    [TestClass()]
+    public class StarTests
+    {
+        [TestMethod()]
+        public void StarTest()
+        {
+            Star sun = new Star("Sun", 1);
+            if(sun.Name != "Sun") 
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void showInfTest()
+        {
+            Star sun = new Star("Sun", 1);
+            if(sun.showInf(5) != 5)
+            Assert.Fail();
+        }
+
+       
+    }
+}
