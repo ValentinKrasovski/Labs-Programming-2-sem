8d74ad5d411311935680b8d3096cc25ea3739211
ValentinKrasovski <92800455+ValentinKrasovski@users.noreply.github.com>
2022-05-26 01:55:11 +0300

    Add files via upload

---
+++ Lab 4/Lab 4Tests/TariffTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab_4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4.Tests
+{
+    [TestClass()]
+    public class TariffTests
+    {
+        [TestMethod()]
+        public void ChangeTariffSizeUpTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
